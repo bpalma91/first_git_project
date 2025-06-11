6adf015bcebeeda4a5b2037f85abb42ae3629c29
Borys <boryspalma@gmail.com>
2025-06-10 21:54:49 -0400

    Update HelloWorld.java
    added print lines

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println();
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Programmers");
+        
+
+        
 
     }
 }
